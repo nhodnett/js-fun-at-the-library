f4f529aa4db39059d65615c4108892488517bbe4
Nathan Hodnett <nathanhodnett@gmail.com>
2022-02-06 16:58:11 -0700

    Complete Librarian class test

--- src/librarian.js
+++ src/librarian.js
@@ -1,7 +1,30 @@
 class Librarian {
-  constructor() {
-    
-  }
-}
+  constructor(name, library) {
+    this.name = name;
+    this.library = library;
+  };
 
+greetPatron(name, morning) {
+  if (morning === true) {
+    return `Good morning, ${name}!`
+  } else {
+      return `Hello, ${name}!`
+    }
+};
+
+findBook(title) {
+  for (var i = 0; i < this.library.shelves.fantasy.length; i++) {
+    if (this.library.shelves.fantasy[i].title === title) {
+      this.library.shelves.fantasy.splice(i, 1)
+      return `Yes, we have ${title}`
+  }
+      return `Sorry, we do not have ${title}`
+}
+};
+
+calculateLateFee(daysLate) {
+  return Math.ceil(daysLate * .25)
+};
+
+};
 module.exports = Librarian;
